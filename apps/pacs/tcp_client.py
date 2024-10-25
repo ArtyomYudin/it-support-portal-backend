be43112efdc854710bc77a689618ff30a42497c1
Artyom Yudin <a.yudin@center-inform.ru>
2024-10-25 14:03:39 +0300

    pacs: tcp socket fix tls

--- apps/pacs/tcp_client.py
+++ apps/pacs/tcp_client.py
@@ -6,8 +6,7 @@ import ssl
 import struct
 import sys
 import threading
-
-from django.db.models.functions import NullIf
+from time import sleep
 
 logger = logging.getLogger(__name__)
 
@@ -42,17 +41,16 @@ class TcpClient:
             buffer_with_byte[4:] = buffer
             return bytes(buffer_with_byte)
 
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-        #context.options |= (
-        #        ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
-        #)
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        context.options &= ~ssl.OP_NO_TLSv1_3 & ~ssl.OP_NO_TLSv1_2 & ~ssl.OP_NO_TLSv1_1
         context.verify_mode = ssl.CERT_REQUIRED
         context.minimum_version = ssl.TLSVersion.TLSv1
-        context.set_ciphers('AES256-SHA')
+        #context.set_ciphers('AES256-SHA')
+        context.set_ciphers('DEFAULT@SECLEVEL=0')
         context.load_verify_locations(self.server_cert)
         context.load_cert_chain(certfile=self.server_cert, keyfile=self.server_key)
 
-        client = None
+        #client = None
         #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #try:
         #    logger.info('Try to connect')
@@ -64,67 +62,47 @@ class TcpClient:
 
         try:
             client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client = context.wrap_socket(client)
-            #client.settimeout(2.0)
+            #client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            client = context.wrap_socket(client, server_hostname='SKD')
         except socket.error as e:
-            client.close()
-            client = None
+           # client.close()
+            print('1')
+            #client = None
             return logger.warning('Error creating socket: %s' % e)
         try:
-            client.bind(('0.0.0.0', self.SPORT))
+            #client.bind(('172.30.4.133', self.SPORT))
             client.connect((self.HOST, self.PORT))
+            print(client)
         except socket.gaierror as e:
             client.close()
-            client = None
+            print('2')
+            #client = None
             return logger.warning('Address-related error connecting to server: %s' % e)
         except socket.error as e:
             client.close()
-            client = None
+            print('3')
+            #client = None
             return logger.warning('Connection error: %s' % e)
 
         if client is None:
+            print('4')
             logger.info('could not open socket')
         #    #sys.exit(1)
 
-        return client
-
-    def receive_data(self):
-
-        while True:
-            received = None
-            #data = client.recv()
-            #if data:
-            #    received = json.loads(data[4:].decode('utf-8'))
-            #    print(f'RECEIVED: {received}')
-            try:
-                recv_data = client.recv(4096)
-            except:
-                # Handle the case when server process terminates
-                print("Server closed connection, thread exiting.")
-                #thread.interrupt_main()
-                break
-            if not recv_data:
-                # Recv with no data, server closed connection
-                print("Server closed connection, thread exiting.")
-                #thread.interrupt_main()
-                break
-            else:
-                received = json.loads(recv_data[4:].decode('utf-8'))
-                #print("Received data: ", json.loads(recv_data[4:].decode('utf-8')))
-
-        return received
-
         #try:
-        #    client.send(create_buffer(filter_events_command))
-        #    print(client)
-        #    while True:
-        #        data = client.recv()
-        #        if data:
-        #            received = json.loads(data[4:].decode('utf-8'))
-        #            print(f'RECEIVED: {received}')
+            #with client:
+        client.sendall(create_buffer(filter_events_command))
+        #print(create_buffer(filter_events_command))
+        while True:
+            data = client.recv(1024)
+            if data:
+                received = json.loads(data[4:].decode('utf-8'))
+                print(f'RECEIVED: {received}')
+                    #else: print(data)
         #finally:
         #    client.close()
-        #    logger.info('Connection closed')
+        #    print('5')
 
-#pacs_tcp_client = TcpClient()
-#pacs_tcp_client.connect()
+pacs_tcp_client = TcpClient()
+pacs_tcp_client.connect()
+
