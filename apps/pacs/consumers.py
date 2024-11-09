043e2d8a9b9bf45e1e27c5ffaf4be8537969f06f
Artyom Yudin <a.yudin@center-inform.ru>
2024-11-09 22:56:00 +0300

    pacs consumers

--- apps/pacs/consumers.py
+++ apps/pacs/consumers.py
@@ -1,7 +1,7 @@
 import json
 import logging
-from datetime import datetime
-
+from datetime import datetime, timedelta
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer, JsonWebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
 
@@ -10,21 +10,23 @@ from .serializers import EventListSerializer
 
 logger = logging.getLogger(__name__)
 
-class PacsConsumer(WebsocketConsumer):
+class PacsConsumer(JsonWebsocketConsumer):
 
     def current_day_event(self):
-        events = Event.objects.filter(created__gte = datetime.now().date())
+        events = Event.objects.filter(created__gte = datetime.now().date() - timedelta(days=2))
         tt = EventListSerializer(instance=events, many=True)
-        #logger.info(tt.data)
-        return events
+        logger.info(tt.data)
+        return tt.data
 
     def connect(self):
         #user = self.scope['user']
         #if user.is_authenticated:
-            logger.info("Connected to websocket")
-            self.accept()
+        #    logger.info("Connected to websocket")
+        #    self.accept()
         #else:
         #    self.close(code=1000)
+        async_to_sync(self.channel_layer.group_add)("pacs", self.channel_name)
+        self.accept()
 
     def disconnect(self, close_code):
         logger.info("Disconnected to websocket")
@@ -36,14 +38,34 @@ class PacsConsumer(WebsocketConsumer):
         logger.info('!!!!!!!!!!!!!!!!!!!!!!')
         logger.info(response)
         logger.info('!!!!!!!!!!!!!!!!!!!!!!')
-        self.current_day_event()
+        results_data = self.current_day_event()
+        #data = {
+        #    "event": "event_pacs_entry_exit",
+        #    "data": {
+        #        "results": {"eventDate": "2024-11-02 11:48:27", "displayName": "Test Tes Test", "accessPoint": "20"},
+        #        "total": 1
+        #    }
+        #}
+        #results_data = [{"eventDate": "2024-11-02 11:48:27", "displayName": "Test Tes Test", "accessPoint": "20"}]
         data = {
-            "event": "event_pacs_entry_exit",
-            "data": {
-                "results": {"eventDate": "2024-11-02 11:48:27", "displayName": "Test Tes Test", "accessPoint": "20"},
-                "total": 1
+            'event': 'event_pacs_entry_exit',
+            'data': {
+                'results': results_data,
+                'total': 1101,
             }
         }
-        send_mess = json.dumps(data)
+
+        async_to_sync(self.channel_layer.group_send)(
+            'pacs',
+            {
+                'type': 'pacs.message',
+                'text': data,
+                'sender': 'Test'
+            },
+        )
+
+    def pacs_message(self, event):
+        self.send_json(event['text'])
+        #send_mess = json.dumps(data)
         #print(send_mess)
-        #self.send(data)
+        #self.send('data')
--- apps/pacs/serializers.py
+++ apps/pacs/serializers.py
@@ -3,22 +3,27 @@ from .models import CardOwner, Event, AccessPoint
 
 
 class CardOwnerSerializer(serializers.ModelSerializer):
+    display_name = serializers.SerializerMethodField()
     class Meta:
         model = CardOwner
-        fields = ['system_id', 'lastname', 'firstname', 'secondname']
+        fields = ['system_id', 'lastname', 'firstname', 'secondname', 'display_name']
+
+    def get_display_name(self, obj):
+        return f'{obj.lastname} {obj.firstname} {obj.secondname}'
 
 class AccessPointSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccessPoint
-        fields = ['system_id', 'name']
+        fields = ['name']
 
 class EventListSerializer(serializers.ModelSerializer):
-    ap_id = AccessPointSerializer()
-    owner_id = CardOwnerSerializer()
+    #ap_id = AccessPointSerializer()
+    #owner_id = CardOwnerSerializer()
+    #owner_name = serializers.SerializerMethodField()
+    displayName = serializers.CharField(source='owner_id.lastname', read_only=True)
+    accessPoint = serializers.CharField(source='ap_id.name', read_only=True)
+    eventDate = serializers.CharField(source='created', read_only=True)
     class Meta:
         model = Event
-        fields = ['created', 'ap_id', 'owner_id']
-
-    #def get_current_day_events(self, instance):
-    #    event = instance.
+        fields = ['eventDate',  'displayName', 'accessPoint']
 
